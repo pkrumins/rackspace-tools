4337e02810ac1c53b896ae71a455a00cb34e8e38
Peteris Krumins <peteris.krumins@gmail.com>
2012-04-17 23:48:41 +0300

    remove unnecessary checks. read correct config

--- bin/list-servers.js
+++ bin/list-servers.js
@@ -1,30 +1,15 @@
 var fs = require('fs');
 var rackspace = require('cloudservers');
 
-var configStats = fs.statSync('./config.json');
-if (!configStats.isFile()) {
-    console.log("Config file not found");
-    process.exit(1);
-}
-
-var config = JSON.parse(fs.readFileSync('./config.json'));
+var config = JSON.parse(fs.readFileSync(__dirname + '../config.json'));
 var cloudServerConfig = {
     auth : config
 };
 
 var client = rackspace.createClient(cloudServerConfig);
-/*
-client.setAuth(function (err) {
-    if (err) {
-        console.log("Failed authenticating at RackSpace. Error:", err.toString());
-        process.exit(1);
-    }
-    */
-    client.getServers(true, function (err, servers) {
-        servers.forEach(function (server) {
-            console.log(server.name + ": " + server.addresses.public);
-        });
+client.getServers(true, function (err, servers) {
+    servers.forEach(function (server) {
+        console.log(server.name + ": " + server.addresses.public);
     });
-/*
 });
-*/
+
